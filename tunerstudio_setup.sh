778b6e1ac7717caad422d37776f5150e04c25df2
Rob Cl <rclandrover@gmail.com>
2024-09-27 12:29:58 +0100

    Update tunerstudio_setup.sh

--- tunerstudio_setup.sh
+++ tunerstudio_setup.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
-$ tar -xvzf documents.tar.gz -C /tmp/files
+wget https://www.efianalytics.com/TSDash/download/TSDash_v0.9.09.02_Beta.tar.gz
+
+wget https://www.efianalytics.com/TunerStudio/beta/TunerStudioMS_v3.2.03.00_Beta.tar.gz
+
+x=$( pwd )
+
+tar -xvzf TSDash_v*.tar.gz -C $x/../
+
+tar -xvzf TunerStudioMS_*.tar.gz -C $x/../
+
+cd ../
+
+x=$( pwd )
+
+sudo chmod +777 $x/TSDash/TSDash.sh
+sudo chmod +777 $x/TunerStudioMS/TunerStudio.sh
